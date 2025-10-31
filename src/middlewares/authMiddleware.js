1a5cbc7cecc764d12203380a2451f65be69a8dc8
covaikvk <covaikvk@gmail.com>
2025-10-31 10:21:45 +0530

    auth middle ware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -6,12 +6,14 @@ function authMiddleware(req, res, next) {
   if (!token) return res.status(401).json({ msg: "No token, authorization denied" });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // { id: user.id }
+    // Ignore expiration Here ✅
+    const decoded = jwt.verify(token, process.env.JWT_SECRET, { ignoreExpiration: true });
+    
+    req.user = decoded; 
     next();
   } catch (err) {
-    res.status(401).json({ msg: "Token is not valid" });
+    return res.status(401).json({ msg: "Token is not valid" });
   }
 }
 
-module.exports = authMiddleware; // ✅ make sure this line exists and is NOT commented
+module.exports = authMiddleware;
